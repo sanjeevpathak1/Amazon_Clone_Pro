ef7351c69383294bba04d1e8ad23ef28dda4b9d7
sanjeevpathak1 <sanjeevkumar13901@gmail.com>
2025-04-11 22:17:40 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,15 @@
+let btn=document.querySelector(".search-select");
+btn.addEventListener("click",()=>
+{
+    console.log("buton was clicked");
+});
+btn.setAttribute("houre",()=>
+{
+    console.log("new add");
+});
+let menu=document.querySelector(".panel-all");
+menu.addEventListener("click",()=>
+{
+    hello;
+    jey;
+});
